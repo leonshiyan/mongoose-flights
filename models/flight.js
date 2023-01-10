876cdb44ba108d9aa62ae1d6452672ee53908646
leonshiyan <leonshiyan@gmail.com>
2023-01-09 21:38:39 -0800

    set up partial

--- models/flight.js
+++ models/flight.js
@@ -28,3 +28,9 @@ const flightSchema = new Schema({
 }, {
   timestamps: true
 })
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ import { Router } from 'express'
 
 const router = Router()
 
-/* GET home page. */
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Mongoose Flights' })
 })
 
 export { 
